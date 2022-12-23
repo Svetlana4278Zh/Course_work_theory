e8908c90b7d428335efe88c40193533be764f8b6
Svetlana4278Zh <svetlana.zh@inbox.ru>
2022-12-23 22:51:44 +0300

    Ticket 2, task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String string = "А роза упала на лапу Азора";
+        System.out.println(isPalindrom(string));
+    }
+
+    public static boolean isPalindrom(String text) {
+        text = text.replace(" ", "");
+        text = text.toLowerCase();
+        boolean palindrom = true;
+        for (int i = 0; i < text.length()/2; i++) {
+            if (text.charAt(i) != text.charAt(text.length() - 1 - i)) {
+                palindrom = false;
+            }
+        }
+        return palindrom;
     }
 }
