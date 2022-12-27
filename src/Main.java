44d604b434037f5c607b80f4f31a8c8060f64a19
Svetlana4278Zh <svetlana.zh@inbox.ru>
2022-12-27 22:20:00 +0300

    Ticket 2, task

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String string = "А роза упала на лапу Азора";
+        String string = "Анна";
         System.out.println(isPalindrom(string));
     }
 
